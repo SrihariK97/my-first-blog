7068038f03334eede301a16521efd91d1e5bf85f
hari <sri381997@gmail.com>
2021-01-28 16:22:26 +0530

    Added user's comment section for the blog post inside the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,3 +1,14 @@
+from django.urls import path, include
+from django.contrib import admin
+
+from django.contrib.auth import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('', include('blog.urls')),
+]
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,10 +24,4 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
-]
